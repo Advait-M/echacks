a91d58ee86441c883c1760d7e109551361a1f5f8
Misha Kuzma <mishakuzma@gmail.com>
2016-11-04 23:59:17 -0400

    Test commit

---
+++ userDataClass.py
@@ -0,0 +1,29 @@
+def UserData():
+    def __init__(self):
+        self.opinionString = ""
+        self.politicalParty = ""
+        self.mood = ""
+
+    def __str__(self):
+        return "Opinion: " + self.opinionString +"\nParty:"+self.politicalParty+"\nMood: "+self.mood
+
+    # updateOpinion takes self and a string called opiString and mutates self so that
+    #   the string that is sent to indico is updated with the latest tweet
+    # updateMood: UserDate Str -> None
+    # Effects: Mutates self
+    def updateOpinion(self, opiString):
+        self.opinionString.append(" " + opiString)
+
+    # updateParty takes self and a string called inParty and mutates the party
+    #   element of self to inParty, which is received from indico.
+    # updateMood: UserDate Str -> None
+    # Effects: Mutates self
+    def updateParty(self, inParty):
+        self.politicalParty = inParty
+
+    # updateMood takes self and a string called inMood and mutates the mood property
+    #   of self to inMood, which is received from indico.
+    # updateMood: UserDate Str -> None
+    # Effects: Mutates self
+    def updateMood(self, inMood):
+        self.mood = inMood
