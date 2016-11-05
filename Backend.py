91fbff6b10aa9d011cf8b19ced6de8fed6acb7d5
Advait Maybhate <advait.maybhate9@gmail.com>
2016-11-05 16:37:40 -0400

    Stopped some twtter handles breaking the program

--- Backend.py
+++ Backend.py
@@ -1,35 +1,40 @@
 from twitter import *
+import time
+config = {}
+with open("config.py") as f:
+    code = compile(f.read(), "config.py", 'exec')
+    exec(code, config)
+f.close()
+twitter = Twitter(
+auth=OAuth(config["access_key"], config["access_secret"], config["consumer_key"], config["consumer_secret"]))
 def getTweets(handle):
 
-    config = {}
-    with open("config.py") as f:
-        code = compile(f.read(), "config.py", 'exec')
-        exec(code, config)
-    f.close()
-    twitter = Twitter(auth=OAuth(config["access_key"], config["access_secret"], config["consumer_key"], config["consumer_secret"]))
+    print(handle)
     results = twitter.statuses.user_timeline(screen_name = handle)
-    # print(results)
-    # print()
-    statii = []
-    print(results)
-    print(results[0])
-    uName = results[0]["user"]["name"]
-    print(uName)
-    for status in results:
-        statii.append(status["text"])
-    # print(statii)
-    textS = " ".join(statii)
-    while "\n" in textS:
-        i = textS.index("\n")
-        textS = textS[0:i] + textS[i+1:]
+    if results != []:
+        # print(results)
+        # print()
+        statii = []
+        print(results)
+        #print(results[0])
+        uName = results[0]["user"]["name"]
+        print(uName)
+        for status in results:
+            statii.append(status["text"])
+        # print(statii)
+        textS = " ".join(statii)
+        while "\n" in textS:
+            i = textS.index("\n")
+            textS = textS[0:i] + textS[i+1:]
 
-    print(textS)
-    print([uName, textS])
-    return [uName, textS]
+        print(textS)
+        print([uName, textS])
+        return [uName, textS]
+    else:
+        return[]
 if __name__ == "__main__":
     politicallyActiveHandles = [
         "RepublicanStudy",
-        "benpolitico",
         "daveweigel",
         "fixfelicia",
         "pwire",
@@ -76,4 +81,4 @@ if __name__ == "__main__":
         "MysteryPollster",
         "RasmussenPoll"]
 
-    getTweets("POTUS")
+    # for i in range(0, 10):
