f52401c6a2dcb3e5e744ac69bf2cacaff420e5a7
Advait Maybhate <advait.maybhate9@gmail.com>
2016-11-05 20:20:08 -0400

    update flask

--- test flask.py
+++ test flask.py
@@ -7,5 +7,7 @@ def index():
     data = pydb.LogiticaPolitica().getAll()
     #data = "asdad"
     return render_template("index.html", data = data)
-app.run()
-pydb.LogiticaPolitica().start()
+if __name__ == "__main__":
+
+    app.run()
+    pydb.LogiticaPolitica().start()
